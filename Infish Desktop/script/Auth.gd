27e23d4fd10cf49d37b2dcdb5dfcbaa8a6e0ad7d
darkun7 <182410101113@students.unej.ac.id>
2020-04-02 23:41:27 +0700

    Transition between scene

--- Infish Desktop/script/Auth.gd
+++ Infish Desktop/script/Auth.gd
@@ -1,25 +1,22 @@
 extends Control
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_btn_goto_login_button_up():
-	var path = "MarginContainer/split/r_side/vcontainer/"
+	var path = "container/split/r_side/vcontainer/"
 	get_node(path+"panel_register").visible = false
 	get_node(path+"panel_login").visible = true
 
-
 func _on_btn_goto_register_button_up():
-	var path = "MarginContainer/split/r_side/vcontainer/"
+	var path = "container/split/r_side/vcontainer/"
 	get_node(path+"panel_login").visible = false
 	get_node(path+"panel_register").visible = true
+
+func _on_btn_register_button_up():
+	$PopUP.open("sukses","Auth","Anda berhasil melakukan pendaftara.")
+
+
+func _on_btn_login_button_up():
+	#login
+	pass # Replace with function body.
